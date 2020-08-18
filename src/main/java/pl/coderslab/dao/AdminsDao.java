299d6bf99763eaa2795f769473527cbcd02f079e
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-08-18 17:49:32 +0200

    I corrected the AdminsDao class

--- src/main/java/pl/coderslab/dao/AdminsDao.java
+++ src/main/java/pl/coderslab/dao/AdminsDao.java
@@ -1,11 +1,13 @@
 package pl.coderslab.dao;
 
 import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.exception.NotFoundException;
 import pl.coderslab.model.Admins;
 import pl.coderslab.utils.DbUtil;
 
 import java.sql.*;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminsDao {
 
@@ -17,51 +19,55 @@ public class AdminsDao {
 
     public Admins create(Admins admins) {
         try (Connection conn = DbUtil.getConnection()) {
-            PreparedStatement statement = conn.prepareStatement(CREATE_ADMINS_QUERY, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement statement = conn.prepareStatement(CREATE_ADMINS_QUERY, PreparedStatement.RETURN_GENERATED_KEYS);
             statement.setString(1, admins.getFirstName());
             statement.setString(2, admins.getLastName());
             statement.setString(3, admins.getEmail());
             statement.setString(4, hashPassword(admins.getPassword()));
             statement.setInt(5, admins.getSuperadmin());
             statement.setInt(6, admins.getEnable());
-            statement.executeUpdate();
-            ResultSet resultSet = statement.getGeneratedKeys();
-            if (resultSet.next()) {
-                admins.setId(resultSet.getInt(1));
+            int result = statement.executeUpdate();
+            if (result != 1) {
+                throw new RuntimeException("Execute update returned " + result);
             }
-            return admins;
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if (resultSet.first()) {
+                    admins.setId(resultSet.getInt(1));
+                    return admins;
+                } else {
+                    throw new RuntimeException("Generated key was not found");
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
-            return null;
         }
+        return null;
     }
 
     public String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
-    public Admins read(int userId) {
+    public Admins read(int adminId) {
+        Admins admins = new Admins();
         try (Connection connection = DbUtil.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(READ_ADMINS_QUERY);
-            preparedStatement.setInt(1, userId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            Admins admins = new Admins();
-            if (resultSet.next()) {
-                admins.setFirstName(resultSet.getString("first_name"));
-                admins.setLastName(resultSet.getString("last_name"));
-                admins.setEmail(resultSet.getString("email"));
-                admins.setPassword(resultSet.getString("password"));
-                admins.setSuperadmin(resultSet.getInt("superadmin"));
-                admins.setEnable(resultSet.getInt("enable"));
-                admins.setId(resultSet.getInt("id"));
-            } else {
-                return null;
+            preparedStatement.setInt(1, adminId);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    admins.setFirstName(resultSet.getString("first_name"));
+                    admins.setLastName(resultSet.getString("last_name"));
+                    admins.setEmail(resultSet.getString("email"));
+                    admins.setPassword(resultSet.getString("password"));
+                    admins.setSuperadmin(resultSet.getInt("superadmin"));
+                    admins.setEnable(resultSet.getInt("enable"));
+                    admins.setId(resultSet.getInt("id"));
+                }
             }
-            return admins;
         } catch (SQLException e) {
             e.printStackTrace();
-            return null;
         }
+        return admins;
     }
 
     public void update(Admins admins) {
@@ -85,16 +91,20 @@ public class AdminsDao {
             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ADMINS_QUERY);
             preparedStatement.setInt(1, userId);
             preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+            boolean deleted = preparedStatement.execute();
+            if (!deleted) {
+                throw new NotFoundException("Product not found");
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public Admins[] findAll() {
+    public List<Admins> findAll() {
+        List<Admins> adminsList = new ArrayList<>();
         try (Connection connection = DbUtil.getConnection()) {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(FIND_ALL_ADMINS_QUERY);
-            Admins[] arrayOfAdmins = new Admins[0];
             while (resultSet.next()) {
                 Admins admins = new Admins();
                 admins.setFirstName(resultSet.getString("first_name"));
@@ -104,19 +114,12 @@ public class AdminsDao {
                 admins.setSuperadmin(resultSet.getInt("superadmin"));
                 admins.setEnable(resultSet.getInt("enable"));
                 admins.setId(resultSet.getInt("id"));
-                arrayOfAdmins = addToArray(admins,arrayOfAdmins);
+                adminsList.add(admins);
             }
-            return arrayOfAdmins;
         } catch (SQLException e) {
             e.printStackTrace();
-            return null;
         }
-    }
-
-    private Admins[] addToArray(Admins admins, Admins[] arrayOfAdmins) {
-        Admins[] tmpAdmins = Arrays.copyOf(arrayOfAdmins, arrayOfAdmins.length + 1);
-        tmpAdmins[arrayOfAdmins.length] = admins;
-        return tmpAdmins;
+        return adminsList;
     }
 
 }
