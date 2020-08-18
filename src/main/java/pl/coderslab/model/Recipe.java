a3c6a77953579b536479955f63dca914f6a29d13
Patryk CHmielewski <pchmielewski338@gmail.com>
2020-08-18 20:16:55 +0200

    RecipeDao

---
+++ src/main/java/pl/coderslab/model/Recipe.java
@@ -0,0 +1,118 @@
+package pl.coderslab.model;
+
+import java.time.LocalDateTime;
+
+public class Recipe {
+    private int ID,preparationTime,adminId;
+    private String name, ingredients, description, created,updated, preparation;
+
+    public Recipe(String name, String ingredients, String description, String created,int preparationTime,String preparation,int adminId) {
+        this.preparation = preparation;
+        this.adminId = adminId;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.description = description;
+        this.created = created;
+        this.preparationTime = preparationTime;
+    }
+
+    public Recipe(String name, String ingredients, String description,int preparationTime,String preparation,int adminId) {
+        this.preparation = preparation;
+        this.adminId = adminId;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.description = description;
+        this.preparationTime = preparationTime;
+    }
+
+    public Recipe() {
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getPreparation() {
+        return preparation;
+    }
+
+    public void setPreparation(String preparation) {
+        this.preparation = preparation;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(int preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "ID=" + ID +
+                ", preparationTime=" + preparationTime +
+                ", adminId=" + adminId +
+                ", name='" + name + '\'' +
+                ", ingredients='" + ingredients + '\'' +
+                ", description='" + description + '\'' +
+                ", created='" + created + '\'' +
+                ", updated='" + updated + '\'' +
+                ", preparation='" + preparation + '\'' +
+                '}';
+    }
+}
