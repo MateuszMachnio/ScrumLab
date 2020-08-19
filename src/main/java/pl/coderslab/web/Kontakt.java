8807a71f337e48623ef7b11eacd3bd5cc54bb719
Mateusz Machnio <62938388+MateuszMachnio@users.noreply.github.com>
2020-08-19 14:28:52 +0200

    Update Kontakt.java
    correction

--- src/main/java/pl/coderslab/web/Kontakt.java
+++ src/main/java/pl/coderslab/web/Kontakt.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Kontakt", value = "/kontakt")
+@WebServlet(name = "Kontakt", value = "/kontakt") //lepiej contact jak już piszemy po English;)
 public class Kontakt extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
