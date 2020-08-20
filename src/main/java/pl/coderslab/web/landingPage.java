d8b3900a2bc63e1763937bfb7c1bbe1073fe147d
Patryk CHmielewski <pchmielewski338@gmail.com>
2020-08-20 19:11:55 +0200

    fix landing page

--- src/main/java/pl/coderslab/web/landingPage.java
+++ src/main/java/pl/coderslab/web/landingPage.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "landingPage",value = "")
+@WebServlet(name = "landingPage",value = "/home")
 public class landingPage extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
