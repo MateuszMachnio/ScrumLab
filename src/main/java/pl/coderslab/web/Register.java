44c735e8ad820b657c6c4a2fdd24455a89791fc1
SzymonTomalik <stomalik@gmail.com>
2020-08-26 01:34:03 +0200

    go to register only when unlogged

--- src/main/java/pl/coderslab/web/Register.java
+++ src/main/java/pl/coderslab/web/Register.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -46,6 +47,9 @@ public class Register extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.sendRedirect("/register.jsp");
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loggedUser") == null) {
+            getServletContext().getRequestDispatcher("/register.jsp").forward(request, response);
+        } else response.sendRedirect("/app");
     }
 }
