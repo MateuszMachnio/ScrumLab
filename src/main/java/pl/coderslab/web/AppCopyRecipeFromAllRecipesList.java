bd62e865f807ca4e7b714d71419853990d3d3ed5
SzymonTomalik <stomalik@gmail.com>
2020-08-27 15:19:14 +0200

    not double recipes when copies to UserRecipesList

--- src/main/java/pl/coderslab/web/AppCopyRecipeFromAllRecipesList.java
+++ src/main/java/pl/coderslab/web/AppCopyRecipeFromAllRecipesList.java
@@ -10,12 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "AddRecipeFromAllRecipesList", value = "/app/recipe/add/copy")
 public class AppCopyRecipeFromAllRecipesList extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-
         String name = request.getParameter("name");
         String description = request.getParameter("description");
         int preparationTime = Integer.parseInt(request.getParameter("preparationTime"));
@@ -25,8 +26,20 @@ public class AppCopyRecipeFromAllRecipesList extends HttpServlet {
         int userId = (int) session.getAttribute("loggedUser");
         Recipe recipe = new Recipe(name,ingredients,description,preparationTime,preparation,userId);
         RecipeDao recipeDao = new RecipeDao();
-        recipeDao.create(recipe);
-        response.sendRedirect("/app/recipe/list");
+        List<Recipe>allRecipes=recipeDao.findAll();
+        List<Recipe>userRecipes=new ArrayList<>();
+        for (Recipe recipeFromBase:allRecipes) {
+            if (recipeFromBase.getAdminId() == userId) {
+                userRecipes.add(recipeFromBase);
+            }
+        }
+        if (!userRecipes.contains(recipe)) {
+            recipeDao.create(recipe);
+            response.sendRedirect("/app/recipe/list");
+
+        } else {
+            response.sendRedirect("/appDoubleRecipe.jsp");
+        }
 
     }
 
