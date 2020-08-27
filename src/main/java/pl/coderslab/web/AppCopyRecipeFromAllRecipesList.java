f4456ce74358f4da1dccb4f07993bcc67016f47b
SzymonTomalik <stomalik@gmail.com>
2020-08-27 13:49:37 +0200

    copy from recipesList

--- src/main/java/pl/coderslab/web/AppCopyRecipeFromAllRecipesList.java
+++ src/main/java/pl/coderslab/web/AppCopyRecipeFromAllRecipesList.java
@@ -11,30 +11,31 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "AppUpdateRecipe",value = "/app/recipe/edit")
-public class AppUpdateRecipe extends HttpServlet {
+@WebServlet(name = "AddRecipeFromAllRecipesList", value = "/app/recipe/add/copy")
+public class AppCopyRecipeFromAllRecipesList extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
+
         String name = request.getParameter("name");
         String description = request.getParameter("description");
         int preparationTime = Integer.parseInt(request.getParameter("preparationTime"));
         String preparation = request.getParameter("preparation");
         String ingredients = request.getParameter("ingredients");
-        int id = Integer.parseInt(request.getParameter("recipeId"));
         HttpSession session = request.getSession();
         int userId = (int) session.getAttribute("loggedUser");
         Recipe recipe = new Recipe(name,ingredients,description,preparationTime,preparation,userId);
-        recipe.setID(id);
-        System.out.println(id);
         RecipeDao recipeDao = new RecipeDao();
-        recipeDao.update(recipe);
+        recipeDao.create(recipe);
         response.sendRedirect("/app/recipe/list");
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String recipeId = request.getParameter("recipeId");
-        RecipeDao recipeDao = new RecipeDao();
-        request.setAttribute("recipe", recipeDao.read(Integer.parseInt(recipeId)));
-        getServletContext().getRequestDispatcher("/appEditRecipe.jsp").forward(request,response);
+        int recipeId = Integer.parseInt(request.getParameter("recipeId"));
+        RecipeDao recipeDao=new RecipeDao();
+        Recipe recipe=recipeDao.read(recipeId);
+        request.setAttribute("recipe", recipe);
+        getServletContext().getRequestDispatcher("/appCopyRecipeFromAllRecipesList.jsp").forward(request, response);
+
     }
 }
